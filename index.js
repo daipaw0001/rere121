94081078adb4398caa3dbebba5b3942b14c1dd85
daipaw0001 <31316610+daipaw0001@users.noreply.github.com>
2017-12-30 15:10:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const puppeteer = require('puppeteer');
 function kiusi(){
   (async () => {
   console.log('-----kiusi00000-----------');
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
   await page.goto('https://cnhv.co/htmx'); 
   })();
