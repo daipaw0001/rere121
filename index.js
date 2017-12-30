8783a5f115173239b73d00c99ce4967dbd71cb2e
daipaw0001 <31316610+daipaw0001@users.noreply.github.com>
2017-12-30 15:30:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
 const http = require('http');  
 
  
-(async () => {
- try {
-      const requestHandler = (request, response) => {  
-        console.log(request.url)
-        response.end('xin chao')
-      }
-
-      const server = http.createServer(requestHandler)
-
-       server.listen(3000, (err) => {  
-        if (err) {
-          return console.log('something bad happened', err)
-        }
-
-        console.log(`ngheee`)
-      })
- }catch (e) {
-        console.log(`sai ok`)
- }
-})();
  
+(async () => { 
+  const server = http.createServer(requestHandler)
+
+  server.listen(process.env.PORT, (err) => {  
+    if (err) {
+      return console.log('something bad happened', err)
+    }
+
+    console.log(`server is listening`)
+  })
+ 
+})();
+
+
+
 const puppeteer = require('puppeteer');
 function kiusi(){
   (async () => {
