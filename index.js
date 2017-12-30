100ac97a0eac299a6647fea8cecc8f8019b93f3c
nguyenkaos <nguyen.caal@gmail.com>
2017-12-30 14:50:47 +0700

    concac

---
+++ index.js
@@ -0,0 +1,50 @@
+const http = require('http');  
+
+ 
+(async () => {
+ try {
+      const requestHandler = (request, response) => {  
+        console.log(request.url)
+        response.end('xin chao')
+      }
+
+      const server = http.createServer(requestHandler)
+
+       server.listen(3000, (err) => {  
+        if (err) {
+          return console.log('something bad happened', err)
+        }
+
+        console.log(`ngheee`)
+      })
+ }catch (e) {
+        console.log(`sai ok`)
+ }
+})();
+ 
+const puppeteer = require('puppeteer');
+function kiusi(){
+  (async () => {
+  console.log('-----kiusi00000-----------');
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://cnhv.co/htmx'); 
+  })();
+}
+
+
+
+function f() {
+    (async () => {
+        console.log('-111111111111-------trong f -----------');
+        kiusi();
+        console.log(Date()); 
+        setTimeout( f, 60000 ); 
+        console.log('--tttttttttt--')
+    })();
+}
+
+
+f();
+ 
+ 
